91e4bfd60a0a5fa0534f8052cc4d78a3615546c6
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2021-07-20 18:43:38 +0200

    a

--- src/main/java/com/example/demo/Api/Main_app.java
+++ src/main/java/com/example/demo/Api/Main_app.java
@@ -1,2 +1,20 @@
-package com.example.demo.Api;public class Main_app {
+package com.example.demo.Api;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("")
+@RestController
+@CrossOrigin("*")
+public class Main_app {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    public String greeting() {
+
+        System.out.println("done");
+        return "The Server is work";
+
+    }
+
 }
